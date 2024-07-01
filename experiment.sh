ca7121ca32db010ac57e126ee8931386e15886e9
Danushka Liyanage <57780422+dliyanage@users.noreply.github.com>
2024-07-01 12:53:03 +1000

    Revert "Add periodic dump / restore of data to the script."

--- experiment.sh
+++ experiment.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-YCSB="bin/ycsb"
-MONGOSHELL="build/install/bin/mongo"
-MONGODUMP="mongodb-tools/bin/mongodump"
-MONGORESTORE="./mongodb-tools/bin/mongorestore"
-
 # Define the workload file and the log file
 WORKLOAD_FILE="./workloads/workloada-extend"
 LOG_FILE="./ycsb_results.log"
@@ -58,7 +53,6 @@ write_result() {
         # Extract operation and third value
         operation=$(echo "$line" | awk '{print $1}' | sed 's/,$//' | tr -d '[]')
         third_value=$(echo "$line" | awk '{print $3}' | sed 's/,$//')
-        r=$((10*($epoch-1)+$run))
         
         run_specific=()
         # Extract throughput
@@ -69,7 +63,7 @@ write_result() {
         done <<< "$overall_output"    
         # Append to the values variable
         if [ $k -eq 1 ]; then
-            values="$r,$phase,$recordcount,$readallfields,$requestdistribution,$operation,$readproportion,$updateproportion,$scanproportion,$insertproportion,$extendproportion,${run_specific[0]},${run_specific[1]},$third_value"
+            values="$epoch,$phase,$recordcount,$readallfields,$requestdistribution,$operation,$readproportion,$updateproportion,$scanproportion,$insertproportion,$extendproportion,${run_specific[0]},${run_specific[1]},$third_value"
             k=$((k+1))
             prev_operation="$operation"
         else
@@ -77,8 +71,8 @@ write_result() {
         fi
         if [ "$prev_operation" != "$operation" ]; then
             # Print the values to the output file
-            echo "$values" > "$OUTPUT_FILE"
-            values="$r,$phase,$recordcount,$readallfields,$requestdistribution,$operation,$readproportion,$updateproportion,$scanproportion,$insertproportion,$extendproportion,${run_specific[0]},${run_specific[1]},$third_value"
+            echo "$values" >> "$OUTPUT_FILE"
+            values="$epoch,$phase,$recordcount,$readallfields,$requestdistribution,$operation,$readproportion,$updateproportion,$scanproportion,$insertproportion,$extendproportion,${run_specific[0]},${run_specific[1]},$third_value"
             prev_operation="$operation"
         fi
     done <<< "$filtered_output"
@@ -88,56 +82,66 @@ write_result() {
 
     # Print completion message
     echo "Arrangement completed. Output saved to $OUTPUT_FILE"
+    
 }
 
 # Step 1: Delete the ycsb database on MongoDB if any
 log "=== Deleting the ycsb database on MongoDB, if any ==="
-$MONGOSHELL ycsb --eval "db.dropDatabase()" | tee -a $LOG_FILE
-
-# Step 2: Execute the load phase
-log "=== Executing the load phase ==="
-phase="load"
-$YCSB load mongodb -s -P $WORKLOAD_FILE > $OUTPUT_CSV
-write_result "TRUE"
-
+#mongo ycsb --eval "db.dropDatabase()" | tee -a $LOG_FILE
+mongosh 
+#use ycsb | tee -a $LOG_FILE
+#db.runCommand( { dropDatabase: 1 } ) | tee -a $LOG_FILE
 # Experiment parameters
-for epoch in $(seq 1 10); do
-    for run in $(seq 1 10); do
-        # Step 3: Setting parameter values for extend phase
-        log "=== Setting parameter values for extend phase ==="
-        perl -i -p -e "s/^extendproportion=.*/extendproportion=$extendproportion_extend/" $WORKLOAD_FILE
-        perl -i -p -e "s/^readproportion=.*/readproportion=$readproportion_extend/" $WORKLOAD_FILE
-        perl -i -p -e "s/^updateproportion=.*/updateproportion=$updateproportion_extend/" $WORKLOAD_FILE
-        perl -i -p -e "s/^scanproportion=.*/scanproportion=$scanproportion_extend/" $WORKLOAD_FILE
-        perl -i -p -e "s/^insertproportion=.*/insertproportion=$insertproportion_extend/" $WORKLOAD_FILE
-        source "$WORKLOAD_FILE"
+for epoch in $(seq 1 100); do
+    # Step 2: Setting parameter values for extend phase
+    log "=== Setting parameter values for extend phase ==="
+    perl -i -p -e "s/^extendproportion=.*/extendproportion=$extendproportion_extend/" $WORKLOAD_FILE
+    perl -i -p -e "s/^readproportion=.*/readproportion=$readproportion_extend/" $WORKLOAD_FILE
+    perl -i -p -e "s/^updateproportion=.*/updateproportion=$updateproportion_extend/" $WORKLOAD_FILE
+    perl -i -p -e "s/^scanproportion=.*/scanproportion=$scanproportion_extend/" $WORKLOAD_FILE
+    perl -i -p -e "s/^insertproportion=.*/insertproportion=$insertproportion_extend/" $WORKLOAD_FILE
+    source "$WORKLOAD_FILE"
 
-        # Step 4: Execute the run phase
-        log "=== Executing the run phase with extendproportion=0.2 and other proportions=0 ==="
-        phase="extend"
-        $YCSB run mongodb -s -P $WORKLOAD_FILE > $OUTPUT_CSV
-        write_result "FALSE"
+    if [ $epoch -eq 1 ]; then
+        # Step 3: Execute the load phase
+        log "=== Executing the load phase with extendproportion=0.2 and other proportions=0 ==="
+        phase="load"
+        ./bin/ycsb.sh load mongodb -s -P $WORKLOAD_FILE > $OUTPUT_CSV
+        if [ $initial -eq 1 ]; then
+            write_result "TRUE"
+            initial=$((initial+1))
+        else
+            write_result "TRUE"
+        fi
+    fi
 
-        # Step 5: Setting parameter values for run phase
-        log "=== Setting parameter values for run phase ==="
-        perl -i -p -e "s/^extendproportion=.*/extendproportion=$extendproportion_postextend/" $WORKLOAD_FILE
-        perl -i -p -e "s/^readproportion=.*/readproportion=$readproportion_postextend/" $WORKLOAD_FILE
-        perl -i -p -e "s/^updateproportion=.*/updateproportion=$updateproportion_postextend/" $WORKLOAD_FILE
-        perl -i -p -e "s/^scanproportion=.*/scanproportion=$scanproportion_postextend/" $WORKLOAD_FILE
-        perl -i -p -e "s/^insertproportion=.*/insertproportion=$insertproportion_postextend/" $WORKLOAD_FILE
-        source "$WORKLOAD_FILE"
-
-        # Step 6: Execute the run phase
-        log "=== Executing the run phase with extendproportion=0 and read/update proportions=0.5 ==="
-        phase="run"
-        $YCSB run mongodb -s -P $WORKLOAD_FILE > $OUTPUT_CSV
-        write_result "FALSE"
-    done
-
-    phase="clean-run"
-    $MONGODUMP --uri="mongodb://localhost:27017/ycsb" --archive | $MONGORESTORE --uri="mongodb://localhost:28018/ycsb" --archive --drop
-    $YCSB run mongodb -s -P $WORKLOAD_FILE -p "mongodb://localhost:28018/ycsb?w=1" > $OUTPUT_CSV
+    perl -i -p -e "s/^extendvaluesize=.*/extendvaluesize=$extendvaluesize_extend/" $WORKLOAD_FILE
+    source "$WORKLOAD_FILE"
+    # Step 4: Execute the run phase
+    log "=== Executing the run phase with extendproportion=0.2 and other proportions=0 ==="
+    phase="extend"
+    ./bin/ycsb.sh run mongodb -s -P $WORKLOAD_FILE > $OUTPUT_CSV
     write_result "FALSE"
-done
 
+    # Step 5: Setting parameter values for run phase
+    log "=== Setting parameter values for run phase ==="
+    perl -i -p -e "s/^extendproportion=.*/extendproportion=$extendproportion_postextend/" $WORKLOAD_FILE
+    perl -i -p -e "s/^readproportion=.*/readproportion=$readproportion_postextend/" $WORKLOAD_FILE
+    perl -i -p -e "s/^updateproportion=.*/updateproportion=$updateproportion_postextend/" $WORKLOAD_FILE
+    perl -i -p -e "s/^scanproportion=.*/scanproportion=$scanproportion_postextend/" $WORKLOAD_FILE
+    perl -i -p -e "s/^insertproportion=.*/insertproportion=$insertproportion_postextend/" $WORKLOAD_FILE
+    source "$WORKLOAD_FILE"
+
+    # Step 6: Execute the run phase
+    log "=== Executing the run phase with extendproportion=0 and read/update proportions=0.5 ==="
+    phase="run"
+    ./bin/ycsb.sh run mongodb -s -P $WORKLOAD_FILE > $OUTPUT_CSV
+    write_result "FALSE"
+
+    # Increment run
+    # run=$((run+1))
+done
+# Increment extendproportion_extend parameter
+#extendproportion_extend=$(echo "$extendproportion_extend + 0.1" | bc)
+    
 log "=== All steps completed. Results are logged in $LOG_FILE ==="
